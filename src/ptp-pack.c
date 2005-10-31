de1bc1a294ebbb1294f93e18534dc7cf399f5a54
kil3r <kil3r@3ee8b3d8-b4bd-41ff-86bd-fa876bf61a1d>
2005-10-31 20:37:55 +0000

    Added copyright notice

--- src/ptp-pack.c
+++ src/ptp-pack.c
@@ -1,3 +1,24 @@
+/* ptp-pack.c
+ *
+ * Copyright (C) 2002-2005 Mariusz Woloszyn <emsi@ipartners.pl>
+ *
+ *  This file is part of libptp2.
+ *
+ *  libptp2 is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  libptp2 is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with libptp2; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
 /* curently this file is included into ptp.c */
 
 static inline uint16_t
