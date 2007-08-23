aad454c3ad31ad6e2a0f2d1f609e855b5207ac59
kil3r <kil3r@3ee8b3d8-b4bd-41ff-86bd-fa876bf61a1d>
2007-08-23 11:03:55 +0000

    * ptp-pack.c: fixed signed/unsigned conversion bug         * release: 1.1.10

--- src/ptp-pack.c
+++ src/ptp-pack.c
@@ -34,7 +34,7 @@ htod32p (PTPParams *params, uint32_t var)
 }
 
 static inline void
-htod16ap (PTPParams *params, char *a, uint16_t val)
+htod16ap (PTPParams *params, unsigned char *a, uint16_t val)
 {
 	if (params->byteorder==PTP_DL_LE)
 		htole16a(a,val); else 
@@ -42,7 +42,7 @@ htod16ap (PTPParams *params, char *a, uint16_t val)
 }
 
 static inline void
-htod32ap (PTPParams *params, char *a, uint32_t val)
+htod32ap (PTPParams *params, unsigned char *a, uint32_t val)
 {
 	if (params->byteorder==PTP_DL_LE)
 		htole32a(a,val); else 
@@ -62,26 +62,26 @@ dtoh32p (PTPParams *params, uint32_t var)
 }
 
 static inline uint16_t
-dtoh16ap (PTPParams *params, char *a)
+dtoh16ap (PTPParams *params, unsigned char *a)
 {
 	return ((params->byteorder==PTP_DL_LE)?le16atoh(a):be16atoh(a));
 }
 
 static inline uint32_t
-dtoh32ap (PTPParams *params, char *a)
+dtoh32ap (PTPParams *params, unsigned char *a)
 {
 	return ((params->byteorder==PTP_DL_LE)?le32atoh(a):be32atoh(a));
 }
 
 #define htod8a(a,x)	*(uint8_t*)(a) = x
-#define htod16a(a,x)	htod16ap(params,a,x)
-#define htod32a(a,x)	htod32ap(params,a,x)
+#define htod16a(a,x)	htod16ap(params,(unsigned char *)a,x)
+#define htod32a(a,x)	htod32ap(params,(unsigned char *)a,x)
 #define htod16(x)	htod16p(params,x)
 #define htod32(x)	htod32p(params,x)
 
 #define dtoh8a(x)	(*(uint8_t*)(x))
-#define dtoh16a(a)	dtoh16ap(params,a)
-#define dtoh32a(a)	dtoh32ap(params,a)
+#define dtoh16a(a)	dtoh16ap(params,(unsigned char *)a)
+#define dtoh32a(a)	dtoh32ap(params,(unsigned char *)a)
 #define dtoh16(x)	dtoh16p(params,x)
 #define dtoh32(x)	dtoh32p(params,x)
 
